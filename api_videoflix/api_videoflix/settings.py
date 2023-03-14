7133e4160700a669f2b176594ced13ef39858140
Eric Bruch <eric.bruch@outlook.de>
2023-03-14 22:05:40 +0100

    added django toolbar

--- api_videoflix/api_videoflix/settings.py
+++ api_videoflix/api_videoflix/settings.py
@@ -53,10 +53,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "core.apps.CoreConfig",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -157,3 +159,9 @@ CACHES = {
         "KEY_PREFIX": "videoflix",
     }
 }
+
+# Internal Ips
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
