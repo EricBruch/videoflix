bae01fa217320d4e88e623d58d35ea641135b0da
Eric Bruch <eric.bruch@outlook.de>
2023-03-19 10:30:55 +0100

    customize how user information in django admin is displayed

--- api_videoflix/user/admin.py
+++ api_videoflix/user/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .models import CustomUser
+from .forms import CustomUserCreationForm
 
 # Register your models here.
 
-admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    add_form = CustomUserCreationForm
+    fieldsets = (
+        *UserAdmin.fieldsets,
+        (
+            "Individuelle Daten",
+            {
+                "fields": (
+                    "custom",
+                    "phone",
+                    "address",
+                )
+            },
+        ),
+    )
