dce0703565f3b36c4f2a0c849ab9eb4ba0da0476
Eric Bruch <eric.bruch@outlook.de>
2023-03-22 22:41:14 +0100

    added overwrite for email template overwrite | no changes yet

--- api_videoflix/api_videoflix/settings.py
+++ api_videoflix/api_videoflix/settings.py
@@ -93,7 +93,7 @@ ROOT_URLCONF = "api_videoflix.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
